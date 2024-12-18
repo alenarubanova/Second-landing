23421f69dfaa8bdf34f4cc07a90e234ccf348514
alenarubanova <llenarubanova@gmail.com>
2024-12-18 01:47:17 +0100

    header

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,17 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
   });
+
+  const nav = document.querySelector('#nav');
+  const navBtn = document.querySelector('#nav-btn');
+  const navBtnImg = document.querySelector('#nav-btn-img');
+  
+  navBtn.onclick = () => {
+    if (nav.classList.toggle('open')){
+      navBtnImg.src = './img/headerImg/backgroundHeader.png';
+      document.body.classList.add('no-scroll'); // Блокировка скролла
+    } else {
+      navBtnImg.src = './img/headerImg/headerOpen.png';
+      document.body.classList.remove('no-scroll'); // Разблокировка скролла
+    }
+  }
